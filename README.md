cc7cade3ce321fcca4985708a49736f48cd6b4ad
ksopyla <sopyla@ermlab.com>
2020-03-17 07:28:00 +0100

    add new learning schedullers and fiil mask test script

--- README.md
+++ README.md
@@ -48,25 +48,74 @@ Do I have to provide
 or trainpref is enough
 
 7. Run training 
+
+
+## AWS p3 prepare
+
+Remove conda
+
 ```
-TOTAL_UPDATES=125000    # Total number of training steps
-WARMUP_UPDATES=10000    # Warmup the learning rate over this many updates
-PEAK_LR=0.0005          # Peak learning rate, adjust as needed
-TOKENS_PER_SAMPLE=512   # Max sequence length
-MAX_POSITIONS=512       # Num. positional embeddings (usually same as above)
-MAX_SENTENCES=16        # Number of sequences per batch (batch size)
-UPDATE_FREQ=16          # Increase the batch size 16x
+rm -rf /home/ubuntu/anaconda3
+```
 
-DATA_DIR=data-bin/wikitext
+install sqllite
 
-fairseq-train --fp16 $DATA_DIR \
-    --task masked_lm --criterion masked_lm \
-    --arch roberta_base --sample-break-mode complete --tokens-per-sample $TOKENS_PER_SAMPLE \
-    --optimizer adam --adam-betas '(0.9,0.98)' --adam-eps 1e-6 --clip-norm 0.0 \
-    --lr-scheduler polynomial_decay --lr $PEAK_LR --warmup-updates $WARMUP_UPDATES --total-num-update $TOTAL_UPDATES \
-    --dropout 0.1 --attention-dropout 0.1 --weight-decay 0.01 \
-    --max-sentences $MAX_SENTENCES --update-freq $UPDATE_FREQ \
-    --max-update $TOTAL_UPDATES --log-format simple --log-interval 1
+```
+sudo apt install libsqlite3-dev
+```
+
+intall pyenv
+
+```
+curl https://pyenv.run | bash
+```
+
+install python 3.7.3
+
+```
+pyenv install 3.7.3
+
+python --version
+```
+Set global python version to 3.7.3
+
+```
+pyenv global 3.7.3
+```
 
 
+Install pipenv - check 
+
+```
+pip install --user pipenv
+```
+
+clone the herbert repo
+
+```
+git clone https://github.com/Ermlab/herbert.git
+git checkout dev
+```
+
+Install dependecies
+
+```
+cd herbert
+pipenv install
+```
+
+run trainning
+
+run tensorboard
+
+```
+tensorboard --logdir /mnt/efs/fs1/bert_model/checkpoints/wiki_model/logs/
+```
+
+
+
+Tunell ssh to Tensorboard
+```
+ssh -A -t [user]@[remote_server] -L [local_port]:localhost:[remote_port]
+```
     
---
+++ fill_mask_task.py
@@ -0,0 +1,70 @@
+#%%
+import os
+
+from fairseq.models.roberta import RobertaModel, RobertaHubInterface
+from fairseq import hub_utils
+
+
+# OPI model
+root_path = "./data/external_models/opi/"
+model_path = os.path.join(root_path)
+checkpoint_file = "checkpoint_best.pt"
+
+vocab_model_file="sentencepiece.model"
+vocab_path = os.path.join(root_path,vocab_model_file)
+
+
+#%% Ermlab model
+root_path = "./data/wiki_model/"
+model_path = os.path.join(root_path,"checkpoints/")
+checkpoint_file = "checkpoint94.pt"
+checkpoint_file = "checkpoint77.pt"
+
+vocab_model_file="wikipedia_upper_voc_32000_sen10000000.model"
+vocab_path = os.path.join(root_path, "vocab", vocab_model_file)
+#%%
+
+loaded = hub_utils.from_pretrained(
+    model_name_or_path=model_path,
+    checkpoint_file=checkpoint_file,
+    data_name_or_path='./',
+    bpe="sentencepiece",
+    sentencepiece_vocab=vocab_path,
+    load_checkpoint_heads=True,
+    archive_map=RobertaModel.hub_models(),
+    cpu=True
+)
+roberta = RobertaHubInterface(loaded['args'], loaded['task'], loaded['models'][0])
+roberta.eval()
+
+#%%
+
+def print_mask(s, predicted):
+
+    print(s)
+    for p in predicted:
+        print(f'\t{p[2]} - {p[0]} - confidence {p[1]}')
+
+sentences = [
+    # 'Bolesław Bierut objął rządy w <mask> roku.', #1948
+    # 'Największym <mask> we współczesnym świecie jest głód.',
+    # 'Wikipedia powstała jako projekt uzupełniający dla <mask>, darmowej encyklopedii internetowej',  #Nupedii
+    # 'W Olsztynie pracował Mikołaj Kopernik, ten który <mask> ziemię a wstrzymał słońce.',
+    # 'Krzysztof <mask> do sklepu i zrobił zakupy na śniadanie.',
+    # 'Anna <mask> do sklepu i zrobiła zakupy na śniadanie.',
+    'Samolot wystartował i zjadł <mask>.',
+    'Nie lubił zupy <mask> , ale musiał ją zjeść i wtedy zwymiotował',
+    'Czytał jedząc <mask> i wtedy pojawili się kosmici',
+    'Na śniadanie zjadł kanapkę z <mask>, marchewką i sałatą',
+    'Na śniadanie zjadł kanapkę z <mask>, sałatą i marchweką',
+    'Na śniadanie zjadł kanapkę z pomidorem, marchewką, <mask> i sałatą',
+    
+
+]
+
+for s in sentences:
+    topk_tokens = roberta.fill_mask(s, topk=5)
+    print_mask(s, topk_tokens)
+
+
+# %%
