35607d245e62509c4f955a9acc152663a9518047
ksopyla <sopyla@ermlab.com>
2020-05-16 11:33:29 +0200

    preper process_sentence for odra processing

--- process_sentences.py
+++ process_sentences.py
@@ -3,6 +3,7 @@ import text_utils as tu
 
 from pathlib import Path
 from tqdm import tqdm
+import datetime as dt
 
 from collections import namedtuple
 
@@ -27,6 +28,9 @@ corpus_tuple = CorpusProcessingTuple(
 )
 
 
+
+
+
 input_file = corpus_tuple.file_path
 p = Path(input_file)
 output_file = f"{p.with_suffix('')}_lines_sentence_pl2.txt"
@@ -41,10 +45,38 @@ stats, vl, pl = tu.corpus_process_sentence(
     max_sentence_length=700,
 )
 #%%
+
+
+
+corpus_oscar_raw = "./data/corpus_raw/corpus_oscar_100k.txt"
+
+
+p = Path(corpus_oscar_raw)
+corpus_oscar_lines = f"{p.with_suffix('')}_lines.txt"
+
+
+
+print(f'Start preparing Oscar corpus')
+print(f"in file={corpus_oscar_raw}\nout file={corpus_oscar_lines}")
+start= dt.datetime.now()
+print(f'Start time: {start}')
+
+
+stats, vl, pl = tu.corpus_process_sentence(
+    corpus_oscar_raw,
+    corpus_oscar_lines,
+    split_each_line_as_doc=True,
+    check_valid_sentence=True,
+    check_lang_sentence=True,
+    max_sentence_length=700,
+)
+
+end= dt.datetime.now()
+print(f'Finish. End time: {end} Start time: {start} took={end-start}')
+
 from pprint import pprint
 
-print(f"\nStatystyki=")
+print(f"Cleaning stats")
 pprint(stats)
 
 #%%
-
